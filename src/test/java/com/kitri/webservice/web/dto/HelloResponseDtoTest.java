4819e4e04792117a2b37eb3846da6d58996541ae
captain-yun <jslovejs182@gmail.com>
2022-12-01 18:51:39 +0900

    - HelloController 및 HelloResponseDto 추가 - lombok 추가 - 해당 테스트 코드도 추가 * lombokable_test는 fail => "해결해야함!"

---
+++ src/test/java/com/kitri/webservice/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.kitri.webservice.web.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokable_test() {
+        //given
+        String name = "test";
+        int amount = 1000;
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+}
